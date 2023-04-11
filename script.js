732fa993b47cb4276c23ceef71f47e891a69dc13
Gabriel Monteiro <gabrieldealmeidamonteiro@gmail.com>
2023-04-11 16:31:58 -0300

    finalizando projeto start da onebitcode[

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function calcular() {
-    let valorConta = document.querySelector("bill").value;
-    let servico = document.getElementById("serviceQual");
-    servico = servico.options[Selection.selectedIndex].value;
-    let pessoas = document.querySelector("people").value;
+    let valorConta = document.getElementById("bill").value;
+    let servico = document.getElementById("serviceQual").value;
+    let pessoas = document.getElementById("people").value;
+    console.log(`valor da conta:${valorConta}, servico:${servico}, pessoas:${pessoas}`);
 
     let gorjeta = (valorConta*servico)/pessoas;
 
+    if(valorConta<=0||pessoas<=0||servico==0) {
+        alert("Por favor, preencha os valores corretamente!")
+        return;
+    }
+
     if(pessoas<=1) {
         document.getElementById("gorjeta").innerHTML = `<p>A gorjeta será ${gorjeta}</p>`;
-        console.log("teste");
     } else {
         document.getElementById("gorjeta").innerHTML = `<p>A gorjeta será ${gorjeta} para cada pessoa</p>`;
-        console.log("teste");
     }
 }
